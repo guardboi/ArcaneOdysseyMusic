280c33eb9e990e2423502f88076817f49461c260
Guard <guardboi24@gmail.com>
2026-01-25 11:24:12 -0800

    elius music box

---
+++ MusicBoxes/EliusMusicBox.cs
@@ -0,0 +1,10 @@
+﻿using Terraria.ModLoader;
+
+namespace ArcaneOdysseyMusic.MusicBoxes
+{
+	public class EliusMusicBox : MusicBox
+	{
+		public override int MusicBoxTile => ModContent.TileType<EliusMusicBoxTile>();
+		public override string MusicFilePath => "Music/Elius";
+	}
+}
